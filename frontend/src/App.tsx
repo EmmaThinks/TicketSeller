e9a6200570a7d61b724a1aa877445c4b5f8e1e2d
EmmaThinks <em4xds@proton.me>
2026-03-03 21:10:39 -0600

    minor frontend changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,12 @@ import { Route, Routes, NavLink } from "react-router-dom";
 
 import Home from "./components/home";
 import MainAdmin from "./components/AdminDashboard/main";
+import MainUser from "./components/User/main";
 import Concerts from "./components/Concerts";
 
 function App() {
   const defaultClassNLink = ({ isActive }: { isActive: boolean }) =>
-    `text-center transition-all duration-300 ${isActive ? "w-40 text-2xl" : "w-30"}`;
+    `text-center transition-all duration-300 ${isActive ? "w-40 text-2xl text-blue-400" : "w-30"}`;
 
   return (
     <>
@@ -17,14 +18,19 @@ function App() {
           Home{" "}
         </NavLink>
         <h1>/</h1>
-        <NavLink to="/AdminDashboard" className={defaultClassNLink}>
-          {" "}
-          Admin{" "}
-        </NavLink>
-        <h1>/</h1>
         <NavLink to="/Concerts" className={defaultClassNLink}>
           {" "}
           Conciertos{" "}
+        </NavLink>
+        <h1>/</h1>
+        <NavLink to="/User" className={defaultClassNLink}>
+          {" "}
+          Usuario{" "}
+        </NavLink>
+        <h1>/</h1>
+        <NavLink to="/AdminDashboard" className={defaultClassNLink}>
+          {" "}
+          Admin{" "}
         </NavLink>
         <h1>/</h1>
       </nav>
@@ -33,6 +39,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/AdminDashboard" element={<MainAdmin />} />
         <Route path="/Concerts" element={<Concerts />} />
+        <Route path="/UserDashboard" element={<MainUser />} />
       </Routes>
     </>
   );
